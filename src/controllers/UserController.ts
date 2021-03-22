cdea842de2b35645460c14b2540ae2e4e5f4d2f8
Michael Douglas Soares <michaeldouglas.10.94@gmail.com>
2021-03-22 11:46:45 -0300

    first commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,22 @@
+import  { Request, Response } from 'express';
+
+import EmailService from '../services/EmailService';
+
+const users = [{name: 'Michael',email: 'michael@gmail.com'},{name: 'Ciclano',ameil: 'ciclado@gmail.com'}];
+
+export default {
+    async index(req: Request, res: Response){
+        return res.json(users);
+    },
+
+    async create(req: Request, res: Response){
+        const emailService = new EmailService();
+
+        emailService.sendMail(
+                {name: 'Michael', email: 'michael@gmail.com'},
+                {subject: 'Welcome system', body: 'Seja bem vindo'}
+            );
+            
+            return res.send('Success');
+    }
+}
